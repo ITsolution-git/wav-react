61ac2d5039639f40b9802cb59c67c4041d20ec08
DESKTOP-MUQBT8I\Vinni <volod.vinichuk@gmail.com>
2018-04-27 21:42:48 +0300

    filters - handle user inputs

--- src/components/filters/VoterFilter.js
+++ src/components/filters/VoterFilter.js
@@ -22,20 +22,55 @@ import {
     IsRegisteredInput,
     VoterStatusInput
 } from './shared/FilterInputs';
+import { captainFields } from './shared/SharedHelper';
 
+const voterFields = {
+        email: 'email',
+        firstName: 'firstname',
+        lastName: 'lastname',
+        state: 'state',
+        gender: 'gender',
+        city: 'city',
+        address: 'address',
+        phone: 'phonenumber',
+        isRegistered: 'isRegistered',
+        voterStatus: 'voterStatus'
+    },
+    checkedConst = 'checked';
 
 class VoterFilter extends BaseComponent {
-    state ={
-        firstNameChecked: false,
-        firstName: ''
+    state = {
+        voter: {},
+        captain: {}
     };
 
-    handleChange = (name, value) => {
-
+    handleChange = (name, checked, value, isVoter = true) => {
+        const type = isVoter ? 'voter' : 'captain';
+        this.setState({
+            [type]: {
+                ...this.state[type],
+                [name + checkedConst]: checked,
+                [name]: value
+            }
+        });
     };
 
     onSearchClick = () => {
-        this.loadVoterSearch();
+        let data = {};
+        const { voter, captain } = this.state;
+
+        Object.keys(voterFields).forEach(field => {
+            if (voter[field + checkedConst]) {
+                data[field] = voter[field];
+            }
+        });
+
+        Object.keys(captainFields).forEach(field => {
+           if (captain[field + checkedConst]) {
+               data.captain[field] = captain[field];
+           }
+        });
+        this.loadVoterSearch(data);
     };
 
     loadVoterSearch = () => {
@@ -60,27 +95,27 @@ class VoterFilter extends BaseComponent {
                         <Typography variant='subheading' gutterBottom>
                             Voter
                         </Typography>
-                        <FirstNameInput />
-                        <LastNameInput />
-                        <EmailInput />
-                        <StateInput />
-                        <GenderInput />
-                        <CityInput />
-                        <AddressInput />
-                        <PhoneInput />
-                        <IsRegisteredInput />
-                        <VoterStatusInput />
+                        <FirstNameInput onChange={(checked, value) => this.handleChange(voterFields.firstName, checked, value )} />
+                        <LastNameInput onChange={(checked, value) => this.handleChange(voterFields.lastName, checked, value )}/>
+                        <EmailInput onChange={(checked, value) => this.handleChange(voterFields.email, checked, value )} />
+                        <StateInput onChange={(checked, value) => this.handleChange(voterFields.state, checked, value )} />
+                        <GenderInput onChange={(checked, value) => this.handleChange(voterFields.gender, checked, value )} />
+                        <CityInput onChange={(checked, value) => this.handleChange(voterFields.city, checked, value )} />
+                        <AddressInput onChange={(checked, value) => this.handleChange(voterFields.address, checked, value )} />
+                        <PhoneInput onChange={(checked, value) => this.handleChange(voterFields.phone, checked, value )} />
+                        <IsRegisteredInput onChange={(checked, value) => this.handleChange(voterFields.isRegistered, checked, value )} />
+                        <VoterStatusInput onChange={(checked, value) => this.handleChange(voterFields.voterStatus, checked, value )} />
                         <Typography variant='subheading' gutterBottom>
                             Captain
                         </Typography>
-                        <UsernameInput />
-                        <EmailInput />
-                        <FirstNameInput />
-                        <LastNameInput />
-                        <AddressInput />
-                        <PhoneInput />
-                        <DateOfBirthInput />
-                        <ZipCodeInput />
+                        <UsernameInput onChange={(checked, value) => this.handleChange(captainFields.username, checked, value, false )} />
+                        <EmailInput onChange={(checked, value) => this.handleChange(captainFields.email, checked, value, false )} />
+                        <FirstNameInput onChange={(checked, value) => this.handleChange(captainFields.firstName, checked, value, false )} />
+                        <LastNameInput onChange={(checked, value) => this.handleChange(captainFields.lastName, checked, value, false )} />
+                        <AddressInput onChange={(checked, value) => this.handleChange(captainFields.address, checked, value, false )} />
+                        <PhoneInput onChange={(checked, value) => this.handleChange(captainFields.phone, checked, value, false )} />
+                        <DateOfBirthInput onChange={(checked, value) => this.handleChange(captainFields.dateOfBirth, checked, value, false )} />
+                        <ZipCodeInput onChange={(checked, value) => this.handleChange(captainFields.zipCode, checked, value, false )} />
                         <Button onClick={this.onSearchClick}>Search</Button>
                     </Col>
                     <Col md={8} className='users' >
