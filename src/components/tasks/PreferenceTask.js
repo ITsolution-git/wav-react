6662436c490e9388d60ba23d875eb1c34ab95ee0
DESKTOP-MUQBT8I\Vinni <volod.vinichuk@gmail.com>
2018-05-18 02:12:12 +0300

    preference task completed

--- src/components/tasks/PreferenceTask.js
+++ src/components/tasks/PreferenceTask.js
@@ -10,7 +10,9 @@ import { getStateInfo } from '../../actions/TaskAction'
 import { getTaskData } from "../../helpers/TaskHelper";
 import WhatToDo from './preferenceSteps/WhatToDo';
 import AskVoter from './preferenceSteps/AskVoter';
-import TaskSuccess from './shared/TaskSuccess';
+import getLastStep from './shared/TaskResult';
+import YesNoButtons from './shared/YesNoButtons';
+import RadioButtons from '../shared/inputs/RadioButtons';
 
 class PreferenceTask extends TaskBase {
     constructor(props, context) {
@@ -18,24 +20,86 @@ class PreferenceTask extends TaskBase {
         const { actions, taskData } = this.props;
         const state = taskData.voter_metaData.state;
         actions.getStateInfo(state);
+        this.state = {
+            isSuccess: true,
+            nextEnabled: false,
+            haveContacted: '',
+            votingType: ''
+        };
     }
+
+    getVotingOptions = () => {
+        return [
+            { value: 'byMail', label: 'Vote by mail' },
+            { value: 'earlyVoting', label: 'Early voting' },
+            { value: 'electionDay', label: 'Election day at polling station' }
+        ]
+    };
+
+    getSubComponent  = () => {
+        const {
+            taskData: {
+                voter_metaData: {
+                    firstname = '',
+                    lastname = ''
+            } } = {},
+        } = this.props;
+
+        const {
+            haveContacted,
+            votingType
+        } = this.state;
+
+        if (haveContacted === 'yes') {
+            return {
+                component: <RadioButtons title='How do they prefer to cast their ballot?'
+                                         values={this.getVotingOptions()}
+                                         onChange={val => this.setState({ votingType: val })}
+                                         value={votingType} />,
+                valid: !!votingType
+            }
+        }
+        return {
+            component: <YesNoButtons title={` Were you able to speak with ${firstname} ${lastname} about how they want to cast their ballot?`}
+                                     value={haveContacted}
+                                     onChange={val => {
+                                         const isNo = val === 'no';
+                                         this.setState({
+                                             haveContacted: val,
+                                             nextEnabled: true,
+                                             isSuccess: !isNo
+                                         })
+                                     }} />,
+            valid: !!haveContacted
+        }
+    };
 
     getSteps = () => {
         const {
             taskData: { voter_metaData = {} } = {},
             stateInfo
         } = this.props;
-
+        const {
+            isSuccess,
+            nextEnabled
+        } = this.state;
+        const subComponent = this.getSubComponent();
         return [
             { label: 'What to do', component: <WhatToDo voterData={ voter_metaData } stateInfo={stateInfo} />, valid: true },
             { label: 'Ask voter', component: <AskVoter voterData={ voter_metaData } stateInfo={stateInfo} />, valid: true },
-            { label: 'Success', component: <TaskSuccess data={ this.getTaskData() } />, valid: true }
+            {
+                label: 'Have contact',
+                component: subComponent.component,
+                valid: subComponent.valid,
+                nextEnabled
+            },
+            getLastStep(isSuccess, this.getTaskData() )
         ];
     };
 
     getTaskData = () => {
         const { taskData = {}} = this.props;
-        return { taskid: taskData._id, points: taskData.group_info.value };
+        return { ...this.state, taskid: taskData._id, points: taskData.group_info.value };
     };
 
     render() {
