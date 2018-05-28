763637346cb7f5e5f900482d1f22cda2f78c6fe1
DESKTOP-MUQBT8I\Vinni <volod.vinichuk@gmail.com>
2018-05-28 10:10:25 +0300

    inputs redesign

--- src/components/shared/validatedInputs/InputBase.js
+++ src/components/shared/validatedInputs/InputBase.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Input, { InputLabel } from 'material-ui/Input';
+import { InputLabel } from 'material-ui/Input';
 import { FormControl, FormHelperText } from 'material-ui/Form';
 import Select from 'material-ui/Select';
 import { MenuItem } from 'material-ui/Menu';
@@ -100,59 +100,6 @@ export class TextInput extends InputBase {
             required,
             disabled,
             label,
-            placeholder,
-            defaultValue,
-            fullWidth = true,
-            maxLength = 50
-        } = this.props;
-
-        const {
-            value = defaultValue || '',
-            isValid,
-            error
-        } = this.state;
-
-          return (
-            <FormControl error={!isValid}
-                         required={required}
-                         disabled={disabled}
-                         fullWidth={fullWidth}>
-                <InputLabel>{ label }</InputLabel>
-                <Input value={value}
-                       placeholder={placeholder}
-                       onBlur={this.onFocusOut}
-                       onChange={e => {
-                           const { value } = e.target;
-                           if (value.length <= maxLength) {
-                               this.onChange(e);
-                           }
-                       }} />
-                <FormHelperText classes={{root: 'btw-input-error'}}>{ error }</FormHelperText>
-            </FormControl>
-        );
-    };
-}
-
-export class TextInput1 extends InputBase {
-    state = this.baseState;
-
-    componentWillReceiveProps(props) {
-        this.checkForValidation(props);
-        if (props.defaultValue !== this.props.defaultValue) {
-            this.setState({ value: props.defaultValue || '' })
-        }
-    }
-
-    componentWillMount() {
-        this.onMount();
-    }
-
-    render = () => {
-        const {
-            required,
-            disabled,
-            label,
-            placeholder,
             defaultValue,
             fullWidth = true,
             maxLength = 50
