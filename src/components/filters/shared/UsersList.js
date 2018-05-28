fba1e00bb77aca37fcc918da37efe6c3ebac63b6
LuckyForever <luckyforever42890@gmail.com>
2018-05-29 01:36:48 +0400

    Add delete captain functionality

--- src/components/filters/shared/UsersList.js
+++ src/components/filters/shared/UsersList.js
@@ -4,6 +4,7 @@ import BaseComponent from '../../shared/BaseComponent';
 import Paginator from '../../shared/Paginator';
 import Spinner from '../../shared/Spinner';
 import UserItem from './UserItem';
+import VoterItem from './VoterItem';
 
 export default class UsersList extends BaseComponent {
     state = {
@@ -20,9 +21,14 @@ export default class UsersList extends BaseComponent {
             <div>
                 <Spinner height={300} loading={isFetching} />
                 <div className='user-list'>
-                    { currentUsers.map((user, index) => <UserItem key={index}
-                                                                  isVoter={isVoter}
-                                                                  user={user} />) }
+                    { isVoter ? 
+                        currentUsers.map((user, index) =>   <VoterItem key={index}
+                                                                isVoter={isVoter}
+                                                                user={user} />)
+                        : currentUsers.map((user, index) => <UserItem key={index}
+                                                                isVoter={isVoter}
+                                                                user={user} />)
+                                                                   }
                 </div>
                 <Paginator items={users}
                            onItemsChange={items => this.setState({ currentUsers: items })}/>
