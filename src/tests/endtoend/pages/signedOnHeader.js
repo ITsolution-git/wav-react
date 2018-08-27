5c697dc06ddbd748f15fb3c3d7ddf6dfdf4eecfe
LuckyForever <luckyforever42890@gmail.com>
2018-08-27 21:46:30 +0400

    #40 login/logout automated test

--- src/tests/endtoend/pages/signedOnHeader.js
+++ src/tests/endtoend/pages/signedOnHeader.js
@@ -11,15 +11,20 @@ module.exports = (driver) => {
 
 
 	let signedOnHeader = {
-		signOutNavDropDown: core.automate.By.id('nav-dropDown'),
+		signOutNavDropDown: core.automate.By.id('nav-dropdown'),
 		signOutLink: core.automate.By.linkText('Sign Out')
 	}
 
-	let signOut =  () => {
+	function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	let openMenu = async () => {
 		try {
-			driver.findElement(signedOnHeader.signOutNavDropDown);
-			 driver.findElement(signedOnHeader.signOutNavDropDown).click();
-			 driver.findElement(signedOnHeader.signOutNavDropDown).click();
+			let menu = await driver.wait(core.automate.until.elementLocated(signedOnHeader.signOutNavDropDown), 4000);
+			await timeout(1000)
+			menu.click()
+			await signOut();
 		}catch(error){
 			if (error instanceof core.automate.error.NoSuchElementError){
 				expect(false).to.be.true
@@ -29,7 +34,23 @@ module.exports = (driver) => {
 			}
 		}
 	};
+
+	let signOut = async () => {
+		try {
+			let a_signout = await driver.findElement(core.automate.By.xpath('//*[@id="root"]/div/div/div[1]/div/nav/div/div[2]/ul[2]/li[3]/ul/li[2]/a'));
+			await timeout(100)
+			a_signout.click()
+		}catch(error){
+			if (error instanceof core.automate.error.NoSuchElementError){
+				expect(false).to.be.true
+			}else {
+				console.log(error)
+				expect(false).to.be.true
+			}
+		}
+	}
+
 	return {
-		signOut
+		openMenu
 	}
 }
