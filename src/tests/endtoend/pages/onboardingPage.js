d8bc04eb1d1f9c4c48c2e823db894e2c96e2ee8a
LuckyForever <luckyforever42890@gmail.com>
2018-08-31 02:30:31 +0400

    #24 Build onboarding automate process

--- src/tests/endtoend/pages/onboardingPage.js
+++ src/tests/endtoend/pages/onboardingPage.js
@@ -1,29 +1,37 @@
 let core = require('../core');
-let urlFetcher = require('../support/dataParser');
+let dataParser = require('../support/dataParser');
 let expect = require('chai').expect;
 let helper = require('../support/helper')
 
 module.exports = (driver) => {
 
+	let voters = dataParser.findByTag('new voters').voters
 
 	let registerPage = {
 		city              : helper.By.id('city'),
 		email             : helper.By.id('email'),
-    state             : helper.By.xpath('//*[@id="root"]/div/div/div[2]/div/div[2]/div[2]/div/div/div/div/div[2]/input'),
-    next              : helper.By.xpath('//*[@id="root"]/div/div/div[2]/div/div[8]/div[2]/button')
+    state             : helper.By.css('.state input')
   }
 
   let enterMoreDetailsForVoter =  async (index) => {
     try {
 
       await helper.timeout(300)
-      await driver.findElement(registerPage.email).sendKeys('automate@test' + index + '.com');
-      await driver.findElement(registerPage.city).sendKeys('New York');
-      await driver.findElement(registerPage.state).sendKeys('NY\n');
+      await driver.findElement(registerPage.email).sendKeys( voters[index].email );
+      await driver.findElement(registerPage.city).sendKeys( voters[index].city );
+      await driver.findElement(registerPage.state).sendKeys( voters[index].state + '\n' );
 
-	    await driver.findElement(registerPage.next).click();
-	    let nextBtn = await driver.wait(core.automate.until.elementLocated(helper.By.xpath('//*[@id="root"]/div/div/div[2]/div/div[2]/div/div[2]/button')), 30000);
-	    nextBtn.click();
+      await helper.timeout(300)
+      await helper.selectButton('Next')
+      
+	    let firstVoter = await driver.wait(core.automate.until.elementLocated(helper.By.id('currentVoter0')), 30000);
+      firstVoter.click();
+      
+      await helper.timeout(300)
+      await helper.selectButton('Yes')
+
+      await helper.timeout(300)
+      await helper.selectButton('Next')
 
     } catch(error) {
 
@@ -42,29 +50,8 @@ module.exports = (driver) => {
     
     try {
 
-      await driver.findElement(registerPage.next).click();
-      let nextBtn = await driver.wait(core.automate.until.elementLocated(helper.By.xpath('//*[@id="root"]/div/div/div[2]/div/div[2]/div/div[2]/button')), 30000);
-      nextBtn.click();
-
-    } catch(error) {
-
-      if (error instanceof core.automate.error.NoSuchElementError){
-        console.log(error)
-        expect(false).to.be.true
-      }else {
-        console.log(error)
-        expect(false).to.be.true
-      }
-
-    }
-  }
-
-  let validateFinishProcess = async () => {
-    
-    try {
-
-      let goBackBtn = await driver.wait(core.automate.until.elementLocated(helper.By.xpath('//*[@id="root"]/div/div/div[2]/div/div[8]/div[1]/button')), 30000);
-      goBackBtn.click();
+      await helper.timeout(300)
+      await helper.selectButton('Go back')
 
     } catch(error) {
 
@@ -81,7 +68,6 @@ module.exports = (driver) => {
 
 	return {
     enterMoreDetailsForVoter,
-    validateGotoNextProcess,
-    validateFinishProcess
+    validateGotoNextProcess
 	}
 }
