a5ce3e8b066962439e12b415e36ebb08fa46af63
LuckyForever <luckyforever42890@gmail.com>
2018-09-04 23:25:12 +0400

    #46 Add automate test for voters

--- src/tests/endtoend/pages/dashboardPage.js
+++ src/tests/endtoend/pages/dashboardPage.js
@@ -4,12 +4,13 @@
 
 
 let core = require('../core');
-let urlFetcher = require('../support/dataParser');
+let dataParser = require('../support/dataParser');
 let expect = require('chai').expect;
 let helper = require('../support/helper')
 
 module.exports = (driver) => {
 
+	let voterData = dataParser.findByTag('new voters').voters
 
 	let dashboardPage = {
 		captainsDashboard	: core.automate.By.className('btw-captains-dashboard'),
@@ -20,10 +21,11 @@ module.exports = (driver) => {
 
 	let validateDashboard = async (...args) => {
 		try {
-			driver.findElement(dashboardPage.captainsDashboard);
-			driver.findElement(dashboardPage.taskCard);
-			driver.findElement(dashboardPage.votersCard);
-			driver.findElement(dashboardPage.messageCard);
+			await helper.timeout(400)
+			await driver.findElement(dashboardPage.captainsDashboard);
+			await driver.findElement(dashboardPage.taskCard);
+			await driver.findElement(dashboardPage.votersCard);
+			await driver.findElement(dashboardPage.messageCard);
 		}catch(error){
 			if (error instanceof core.automate.error.NoSuchElementError){
 				expect(false).to.be.true
@@ -48,8 +50,106 @@ module.exports = (driver) => {
 			}
 		}
 	}
+
+	let goToVoterPage = async () => {
+		try {
+			await driver.executeScript(`
+				document.getElementsByClassName('voters')[0].click()
+			`);
+		}catch(error){	
+			if (error instanceof core.automate.error.NoSuchElementError){
+				expect(false).to.be.true
+			}else {
+				console.log(error)
+				expect(false).to.be.true
+			}
+		}
+	}
+
+	let validateVotersPage = async () => {
+		try {
+			await helper.timeout(1000)
+			await driver.findElement(helper.By.className('voters-list'))
+		}catch(error){	
+			if (error instanceof core.automate.error.NoSuchElementError){
+				expect(false).to.be.true
+			}else {
+				console.log(error)
+				expect(false).to.be.true
+			}
+		}
+	}
+
+	let addVoterProcess = async () => {
+		try {
+			await helper.selectButton('Add Voter')
+
+			// Add new voter info
+			await helper.timeout(1000)
+      await driver.findElement(helper.By.id('firstname')).sendKeys( voterData[4].firstname );
+      await driver.findElement(helper.By.id('lastname')).sendKeys( voterData[4].lastname );
+			await driver.findElement(helper.By.id('city')).sendKeys( voterData[4].city );
+			await driver.findElement(helper.By.id('email')).sendKeys( voterData[4].email );
+			await driver.findElement(helper.By.css('.state input')).sendKeys( voterData[4].state + '\n' );
+
+			await helper.selectButton('Add')
+
+			// Select first from match list
+			let firstVoter = await driver.wait(core.automate.until.elementLocated(helper.By.id('currentVoter0')), 30000);
+      firstVoter.click();
+      
+      await helper.timeout(300)
+			await helper.selectButton('Yes')
+			
+		}catch(error){	
+			if (error instanceof core.automate.error.NoSuchElementError){
+				expect(false).to.be.true
+			}else {
+				console.log(error)
+				expect(false).to.be.true
+			}
+		}
+	}
+
+	let validateAddVoterProcess = async () => {
+		try {
+			// Check if it is on voters page
+			await driver.wait(core.automate.until.elementLocated(helper.By.className('voters-list')), 10000);
+
+			// Check last voter is new voter
+			let voters = await driver.findElements(helper.By.css('.name-info'))
+			let name = await voters[voters.length - 1].getText()
+			expect(name).to.equal(voterData[4].firstname + ' ' + voterData[4].lastname)
+
+			// Click remove button
+			let trashes = await driver.findElements(helper.By.css('.fa-trash'))
+			await trashes[trashes.length - 1].click()
+
+			await helper.timeout(300)
+			await helper.selectButton('Yes')
+
+			await helper.timeout(2000)
+
+			// Check if new voter is removed
+			voters = await driver.findElements(helper.By.css('.name-info'))
+			name = await voters[voters.length - 1].getText()
+			expect(name).to.not.equal(voterData[4].firstname + ' ' + voterData[4].lastname)
+		}catch(error){	
+			if (error instanceof core.automate.error.NoSuchElementError){
+				expect(false).to.be.true
+			}else {
+				console.log(error)
+				expect(false).to.be.true
+			}
+		}
+	}
+
 	return {
 		validateDashboard,
-		validateOnboardingResultProcess
+		validateOnboardingResultProcess,
+		goToVoterPage,
+		validateVotersPage,
+		addVoterProcess,
+		validateAddVoterProcess
 	}
 }
--- src/tests/endtoend/tests/functionality_voter_spec.js
+++ src/tests/endtoend/tests/functionality_voter_spec.js
@@ -5,26 +5,27 @@
 const core = require('../core');
 let urlFetcher = require('../support/dataParser');
 let createSignOnPage = require('../pages/signOnPage');
+let createTaskModal = require('../pages/latestTaskModal');
+let createDashboardPage = require('../pages/dashboardPage');
 
 describe('Voter end to end tests', ()=>{
-	let signOnPage;
-
 	const user = urlFetcher.findByTag('end to end happy path');
 
 	beforeEach(()=>{
 		signOnPage = createSignOnPage(core.driver());
+		latestTaskModal = createTaskModal(core.driver());
+		dashboardPage = createDashboardPage(core.driver());
 	});
 
-
-	/*it('Happy Path add voter',  async ()=>{
-
+	it('Happy Path add/remove voter',  async ()=>{
+		await signOnPage.openSignOnPage('staging');
+		await signOnPage.validateSignOnProcess(user.email, user.password);
+		await latestTaskModal.validateWelcomeModalIsDisplayed();
+		await latestTaskModal.dismissModal();
+		await dashboardPage.validateDashboard();
+		await dashboardPage.goToVoterPage();
+		await dashboardPage.validateVotersPage();
+		await dashboardPage.addVoterProcess();
+		await dashboardPage.validateAddVoterProcess();
 	})
-
-	it('Happy Path delete voter',  async ()=>{
-
-	})
-
-	it('Happy Path update voter',  async ()=>{
-
-	})*/
 })
