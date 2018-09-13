46a9c6ee892699250c189d13e42fc0e71ac35c48
Volodymyr Vinichuk <volod.vinichuk@gmail.com>
2018-09-14 02:37:38 +0300

    #76 Each field in the make list page should have a minimum of 2 characters

--- src/components/shared/validatedInputs/FirstNameInput.js
+++ src/components/shared/validatedInputs/FirstNameInput.js
@@ -5,10 +5,12 @@ import { validate } from '../../../utility/InputValidator';
 
 export default class FirstNameInput extends InputBase {
     render () {
+        const { startValidation } = this.props;
         return <TextInput label='First Name'
                           type='text'
                           id='firstname'
-                          validator={value => validate('text', value)}
+                          validator={value => startValidation ? validate('text', value) : true }
+                          validatorError="Minimum of two characters required"
                           name={fields.firstName}
                           {...this.props } />
     }
