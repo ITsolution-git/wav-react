2355372b5b3bed1a0451672d0c139230659967fc
Volodymyr Vinichuk <volod.vinichuk@gmail.com>
2018-10-02 23:01:58 +0300

    task list modals

--- src/components/tasksList/TasksList.js
+++ src/components/tasksList/TasksList.js
@@ -11,10 +11,15 @@ import BaseComponent from '../shared/BaseComponent';
 import { getStateInfo } from '../../actions/TaskAction';
 import ContentLayout from '../layout/ContentLayout';
 import Icon from '../shared/Icon';
+import routes from '../../constants/Routes';
+import Dialog from '../shared/Dialog';
 
 class TaskList extends BaseComponent {
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            showTipsModal: false
+        };
     }
 
     goToTask = (task, taskRoute) => {
@@ -46,7 +51,7 @@ class TaskList extends BaseComponent {
             tasks = [],
             isFetching
         }} = this.props;
-
+        const { showTipsDialog } = this.state;
         const viewProps = this.getViewProps();
 
         return (
@@ -84,14 +89,14 @@ class TaskList extends BaseComponent {
                         <Col md={5} xs={12}>
                             <Row>
                                 <Col md={12} xs={6} id="tile-div">
-                                    <div className="tile">
+                                    <div className="tile" onClick={() => this.setState({ showTipsDialog: true })}>
                                         <div className={viewProps.tileClass}>
                                             Tips for talking <br />
                                             to your friends
                                         </div>
                                     </div>
                                 </Col>
-                                <Col md={12} xs={6} id="tile-div">
+                                <Col md={12} xs={6} id="tile-div" onClick={() => this.onLink(routes.faq)}>
                                     <div className="tile">
                                         <div className={viewProps.tileClass}>
                                             Voting FAQs
@@ -102,6 +107,34 @@ class TaskList extends BaseComponent {
                         </Col>
                     </Row>
                 </div>
+                <Dialog show={showTipsDialog} closeButton onClose={() => this.setState({ showTipsDialog: false })}>
+                    <div id="btw-tips-dialog">
+                        <div id="title" className="title-24-light-blue">
+                            Tips on Talking to Friends Voting
+                        </div>
+                        <div className="text-15-dark-blue-bold">
+                            So you want to help friends vote! Congratulations! You are now a shepherd
+                            of democracy! <br />
+                            <br />
+                            Luckily, lots of folks done studies about what works best when trying to
+                            register a new voter. We've boiled this stuff down to a few handy tips! <br />
+                            <br />
+                            So! Here are the 4 things you want to keep in mind when talking to your friends
+                            about voting: <br />
+                            <br />
+                            Be clear with your friends about how long stuff will take.
+                            A big psychological hurdle to first time voters is the mistaken assumption that registering or voting takes hours and involves lots of red tape.
+                            Making it clear to your friends that they can hop on vote.org and register in 2 minutes (literally) helps to overcome this barrier. <br />
+                            { this.isDesktop() &&
+                             <span>
+                                Know which issues your friends care about and talk about those in the context of voting. Most young voters don’t associate voting with their everyday lives.
+                                But linking voting with things that affect your friends all the time makes it more likely that they’ll view voting as salient to their lives.
+                                If you know your friend works for minimum wage, talk about ballot measures that would increase the minimum wage!
+                                If you know your friend is frustrated about her health care, make it clear that voting for officials with good healthcare policies would make a positive impact on her life.
+                             </span> }
+                        </div>
+                    </div>
+                </Dialog>
             </ContentLayout>
         );
     }
