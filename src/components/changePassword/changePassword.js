0bdebcb7b0c5cd55d6c7089464a0b2c5e123f88d
greatdolphinls <great.dolphin.ls@gmail.com>
2019-06-06 21:46:42 +0300

    B2-127: implement logic and change design in changePassword and forgotPassword form

--- src/components/changePassword/changePassword.js
+++ src/components/changePassword/changePassword.js
@@ -6,163 +6,137 @@ import { Button, TextField, Grid, Typography, Paper } from '@material-ui/core';
 import { changePasswordRequest } from '../../actions/PasswordRequestAction';
 import BaseComponent from '../shared/BaseComponent';
 import routes from '../../constants/Routes';
-// import Button from '../shared/Button';
-import {
-	PasswordInput,
-	TextInput
-} from '../../components/shared/validatedInputs';
 
 class ChangePassword extends BaseComponent {
-	constructor() {
-		super();
-		this.state = {
-			password: '',
-			confirmPassword: '',
-			valid: {
-				password: false,
-				confirmPassword: false
-			},
-			isChangedPassword: true
-		}
-	}
+    constructor() {
+        super();
+        this.state = {
+            password: '',
+            confirmPassword: '',
+            valid: {
+                password: false,
+                confirmPassword: false
+            },
+            isChangedPassword: true
+        }
+    }
 
-	handleChange = (value, isValid, name) => {
-		this.setState({
-			[name]: value,
-			valid: { ...this.state.valid, [name]: isValid }
-		});
-	};
+    handleChange = (value, isValid, name) => {
+        this.setState({
+            [name]: value,
+            valid: { ...this.state.valid, [name]: isValid }
+        });
+    };
 
-	changePassword = () => {
-		const { valid, password, confirmPassword } = this.state;
+    changePassword = () => {
+        const { valid, password, confirmPassword } = this.state;
 
-		console.log(this.props)
-		if (!this.props.location.state.id) {
-			return true;
-		}
+        console.log(this.props)
+        if (!this.props.location.state.id) {
+            return true;
+        }
 
-		if (password !== confirmPassword) {
-			this.setState({
-				valid: {
-					...valid,
-					confirmPassword: false
-				}
-			})
-			return
-		}
+        if (password !== confirmPassword) {
+            this.setState({
+                valid: {
+                    ...valid,
+                    confirmPassword: false
+                }
+            })
+            return
+        }
 
-		if (valid.password) {
+        if (valid.password) {
 
-			let param = this.props.location.state.id, info = {};
+            let param = this.props.location.state.id, info = {};
 
-			info['password'] = password
-			info['confirmPassword'] = confirmPassword
-			info["userid"] = param;
-			return this.props.changePasswordRequest(info);
-		}
-	}
+            info['password'] = password
+            info['confirmPassword'] = confirmPassword
+            info["userid"] = param;
+            return this.props.changePasswordRequest(info);
+        }
+    }
 
-	componentWillReceiveProps(props) {
-		if (props.isChangedPassword) {
-			this.onLink(routes.login, { isReset: true });
-			return;
-		} else {
-			this.setState({ "isChangedPassword": false });
-		}
-	}
+    componentWillReceiveProps(props) {
+        if (props.isChangedPassword) {
+            this.onLink(routes.login, { isReset: true });
+            return;
+        } else {
+            this.setState({ "isChangedPassword": false });
+        }
+    }
 
-	render() {
+    render() {
 
-		const passwordErrorMsg = (
-			<div>
-				<div>At least one special character</div>
-				<div>At least one number</div>
-				<div>At lease one upper case character</div>
-				<div>Minimum of 7 characters</div>
-			</div>
-		);
+        // Todo: It is not used by sergey
+        // const passwordErrorMsg = (
+        //     <div>
+        //         <div>At least one special character</div>
+        //         <div>At least one number</div>
+        //         <div>At lease one upper case character</div>
+        //         <div>Minimum of 7 characters</div>
+        //     </div>
+        // );
+        const { isChangedPassword } = this.state;
 
-		return (
-			<div className="btw-change-password">
-				<Grid container alignItems='center' justify='center'>
-					<Paper className='content'>
-						<Grid container alignItems='center' justify='flex-start'>
-							<Typography className='title'>Reset Password</Typography>
-						</Grid>
-						<TextField
-							className='input'
-							required
-							autoFocus
-							margin="normal"
-							id="email"
-							label="New password"
-							type="password"
-							fullWidth />
-						<TextField
-							className='input'
-							required
-							autoFocus
-							margin="normal"
-							id="email"
-							label="Confirm new password"
-							type="password"
-							fullWidth />
-						<Button
-							className='button'
-							variant='contained'
-							color='primary'
-							fullWidth>
-							Save New Password
+        return (
+            <div className="btw-change-password">
+                <Grid container alignItems='center' justify='center'>
+                    <Paper className='content'>
+                        {!isChangedPassword && <span style={{ fontSize: "12px", color: "red" }}>Password doesn't not reset</span>}
+                        <Grid container alignItems='center' justify='flex-start'>
+                            <Typography className='title'>Reset Password</Typography>
+                        </Grid>
+                        <TextField
+                            className='input'
+                            required
+                            autoFocus
+                            margin="normal"
+                            id="email"
+                            label="New password"
+                            type="password"
+                            name="password"
+                            fullWidth
+                            onChange={this.handleChange} />
+                        <TextField
+                            className='input'
+                            required
+                            autoFocus
+                            margin="normal"
+                            id="email"
+                            label="Confirm new password"
+                            type="password"
+                            name='confirmPassword'
+                            fullWidth
+                            onChange={this.handleChange} />
+                        <Button
+                            className='button'
+                            variant='contained'
+                            color='primary'
+                            fullWidth
+                            onClick={this.changePassword}>
+                            Save New Password
 						</Button>
-						<div className='remember'>
-							Remembered? <Link to={routes.login}><b>Log in</b></Link>.
+                        <div className='remember'>
+                            Remembered? <Link to={routes.login}><b>Log in</b></Link>.
 						</div>
-					</Paper>
-				</Grid>
-			</div>
-			// <div className='btw-change-password btw-verify'>
-			// 	<div className="container">
-			// 		<div className="verify-captain col-lg-9 col-md-12">
-			// 			{ !this.state.isChangedPassword && <div className="warning-red">Password doesn't not reset</div> }
-			// 			<div className="title-32-light-blue">
-			// 				Enter your new password
-			// 			</div>
-			// 			<div className="text-18-dark-blue">
-			// 				<PasswordInput 
-			// 					onChange={this.handleChange}
-			// 					isVoter={false}
-			// 					name="password"
-			// 					uniqueValidationEnabled={false}
-			// 					required />
-			// 				<TextInput label='Confirm Password'
-			// 					type='password'
-			// 					id="confirmPassword"
-			// 					validator={value => value === this.state.password}
-			// 					validatorError='The passwords do not match'
-			// 					onChange={this.handleChange}
-			// 					name='confirmPassword'
-			// 					required />
-			// 			</div>
-			// 			<div id="btn-next">
-			// 				<Button onClick={this.changePassword}>Submit</Button>
-			// 			</div>
-			// 		</div>
-			// 	</div>
-			// </div>
-		);
-	}
+                    </Paper>
+                </Grid>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-	const { isChangedPassword } = state.request;
-	return {
-		isChangedPassword
-	};
+    const { isChangedPassword } = state.request;
+    return {
+        isChangedPassword
+    };
 };
 
 
 const mapDispatchToProps = (dispatch) => ({
-	changePasswordRequest: (info) => dispatch(changePasswordRequest(info))
+    changePasswordRequest: (info) => dispatch(changePasswordRequest(info))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(ChangePassword));
--- src/components/changePassword/forgotPassword.js
+++ src/components/changePassword/forgotPassword.js
@@ -1,182 +1,142 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
-import { forgotPasswordRequest } from '../../actions/PasswordRequestAction';
 import { Button, TextField, Grid, Typography, Paper } from '@material-ui/core';
 
+import { forgotPasswordRequest } from '../../actions/PasswordRequestAction';
 import { validate } from '../../utility/InputValidator';
 import BaseComponent from '../shared/BaseComponent';
 import routes from '../../constants/Routes';
-import Spinner from '../../components/shared/Spinner';
-// import Button from "../shared/Button";
+// TODO: commented by sergey
+// import Spinner from '../../components/shared/Spinner';
+
+const errorMsg = '* Email is not valid *';
+const fieldName = 'email';
 
 class ForgotPassword extends BaseComponent {
-	constructor() {
-		super();
-		this.state = {
-			info: this.getEmptyState(),
-			isValid: this.getEmptyState(true),
-			isUserFound: -1
-		}
-	}
+    constructor() {
+        super();
+        this.state = {
+            info: this.getEmptyState(),
+            isValid: this.getEmptyState(true),
+            isUserFound: -1
+        }
+    }
 
-	getEmptyState = (initValue = '') => {
-		return {
-			email: initValue
-		};
-	};
+    getEmptyState = (initValue = '') => {
+        return {
+            email: initValue
+        };
+    };
 
-	updateFields(field, event) {
-		let info = Object.assign({}, this.state.info);
-		info[field] = event.target.value;
-		this.setState({
-			info: info
-		})
-	}
+    updateFields(field, event) {
+        let info = Object.assign({}, this.state.info);
+        info[field] = event.target.value;
+        this.setState({
+            info: info
+        })
+    }
 
-	validateFields(field, event) {
-		const { info, isValid } = this.state;
-		const { value } = event.target;
-		let validation = { ...isValid };
+    validateFields(field, event) {
+        const { isValid } = this.state;
+        const { value } = event.target;
+        let validation = { ...isValid };
 
-		validation[field] = validate(field, value);
+        validation[field] = validate(field, value);
+        this.setState({ isValid: validation });
+    }
 
-		this.setState({ isValid: validation });
-	}
+    onForgotPassword(event) {
+        const { isValid, info } = this.state;
+        let validation = { ...isValid };
 
-	onForgotPassword(event) {
-		const { isValid, info } = this.state;
-		let validation = { ...isValid };
-		Object.keys(info).forEach(key => {
-			validation[key] = validate(key, info[key]);
-		});
+        Object.keys(info).forEach(key => {
+            validation[key] = validate(key, info[key]);
+        });
+        this.setState({ isValid: validation });
+        return Object.keys(info).some(key => !validation[key])
+            ? true
+            : this.props.forgotPasswordRequest(info.email);
+    }
 
-		this.setState({ isValid: validation });
+    componentWillReceiveProps(props) {
+        if (props.isUserFound) {
+            this.setState({
+                isUserFound: 1
+            })
+        }
 
-		return Object.keys(info).some(key => !validation[key])
-			? true
-			: this.props.forgotPasswordRequest(info.email);
-	}
+        if (props.isUserFound === false) {
+            this.setState({
+                isUserFound: 0
+            })
+        }
+    }
 
-	renderInput = (name, label, inputType, colWidth = 12, errorMsg) => {
-		return (
-			<div className={`form-group col-xs-${colWidth}`}>
-				<label className="pull-left" htmlFor={name}>{label}</label>
-				<input type={inputType} className="input-field"
-					onChange={this.updateFields.bind(this, name)}
-					onBlur={this.validateFields.bind(this, name)} />
-				{!this.state.isValid[name] && <span className="pull-left">{errorMsg}</span>}
-			</div>
-		)
-	};
+    render() {
 
-	componentWillReceiveProps(props) {
-		if (props.isUserFound) {
-			this.setState({
-				isUserFound: 1
-			})
-		}
+        // TODO: It is connected with Spinner. by sergey
+        // const { isFetching } = this.props;
+        const { isValid, isUserFound } = this.state;
 
-		if (props.isUserFound === false) {
-			this.setState({
-				isUserFound: 0
-			})
-		}
-	}
-
-	render() {
-
-		const { error, isFetching } = this.props;
-
-		return (
-			<div className="btw-forgot-password">
-				<Grid container alignItems='center' justify='center'>
-					<Paper className='content'>
-						<Grid container alignItems='center' justify='flex-start'>
-							<Typography className='title'>Reset Password</Typography>
-						</Grid>
-						<Grid container alignItems='center' justify='flex-start'>
-							<Typography className='description'>
-								Enter the email address associated with your account and
+        return (
+            <div className="btw-forgot-password">
+                <Grid container alignItems='center' justify='center'>
+                    <Paper className='content'>
+                        <Grid container alignItems='center' justify='flex-start'>
+                            <Typography className='title'>Reset Password</Typography>
+                        </Grid>
+                        <Grid container alignItems='center' justify='flex-start'>
+                            <Typography className='description'>
+                                Enter the email address associated with your account and
 								we’ll send you a link to create a new Password
 							</Typography>
-						</Grid>
-						<TextField
-							className='input'
-							required
-							autoFocus
-							margin="normal"
-							id="email"
-							label="Email"
-							type="email"
-							// value={this.state.info}
-							onChange={this.updateFields.bind(this, 'email')}
-							fullWidth />
-						<Button
-							className='button'
-							variant='contained'
-							color='primary'
-							onClick={this.onForgotPassword.bind(this, 'onForgotPassword')}
-							fullWidth>
-							Send Verification Link
-						</Button>
-						<div className='remember'>
-							Remembered? <Link to={routes.login}><b>Log in</b></Link>.
-						</div>
-					</Paper>
-				</Grid>
-
-				{/* <div className='btw-forget-password'>
-                    {!this.isMobile() && this.renderBackToHome()}
-                    <div className="intro">
-                        <p className="intro-title">
-                            Enter the email you used to register an account with us.
-							We will send you an email to reset your password
-						</p>
-                    </div>
-                    <br />
-                    {this.state.isUserFound === 0 && <span style={{ fontSize: "18px", color: "red" }}>User doesn't exist</span>}
-                    {this.state.isUserFound === 1 && <span style={{ fontSize: "18px", color: "green" }}>Reset password request sent</span>}
-                    <form>
-                        {this.renderInput('email', 'Email', 'email', 0, error || '* Email is not valid *')}
-                        <br /><br />
-                    </form>
-                    <Row>
-                        <Col xs={6}>
-                            {this.isMobile() && this.renderBackToHome()}
-                        </Col>
-                        <Col md={12} xs={6}>
-                            <div id="btn_signup">
-                                <Button onClick={this.onForgotPassword.bind(this, 'onForgotPassword')}>Send Request</Button>
-                            </div>
-
-                            <Spinner loading={isFetching} size={50} />
-                        </Col>
+                        </Grid>
+                        {isUserFound === 0 && <span style={{ fontSize: "12px", color: "red" }}>User doesn't exist</span>}
+                        {isUserFound === 1 && <span style={{ fontSize: "12px", color: "green" }}>Reset password request sent</span>}
+                        <TextField
+                            className='input'
+                            required
+                            autoFocus
+                            margin="normal"
+                            id="email"
+                            label="Email"
+                            type="email"
+                            onChange={this.updateFields.bind(this, fieldName)}
+                            onBlur={this.validateFields.bind(this, fieldName)}
+                            fullWidth />
+                        {!isValid[fieldName] && <span style={{ fontSize: "12px", color: "red" }}>{errorMsg}</span>}
+                        {/* TODO: Please update Spinner It is not overlay. by Sergey*/}
+                        {/* <Spinner loading={isFetching} size={50} /> */}
                         <Button
+                            className='button'
                             variant='contained'
                             color='primary'
-                            type='submit'
-                        >
-                            Send Request
-                            </Button>
-                    </Row>
-                </div> */}
-			</div >
-		);
-	}
+                            onClick={this.onForgotPassword.bind(this, 'onForgotPassword')}
+                            fullWidth>
+                            Send Verification Link
+						</Button>
+                        <div className='remember'>
+                            Remembered? <Link to={routes.login}>Log in</Link>.
+						</div>
+                    </Paper>
+                </Grid>
+            </div >
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-	const { isUserFound, isFetching } = state.request;
-	return {
-		isUserFound,
-		isFetching
-	};
+    const { isUserFound, isFetching } = state.request;
+    return {
+        isUserFound,
+        isFetching
+    };
 };
 
 
 const mapDispatchToProps = (dispatch) => ({
-	forgotPasswordRequest: (email) => dispatch(forgotPasswordRequest(email))
+    forgotPasswordRequest: (email) => dispatch(forgotPasswordRequest(email))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(ForgotPassword));
