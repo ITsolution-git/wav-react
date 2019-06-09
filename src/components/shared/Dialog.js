ecc16fab829c151389e240c62f504f3e9ef5ac3b
greatdolphinls <great.dolphin.ls@gmail.com>
2019-06-09 21:58:05 +0300

    B2-140: fix styling issues in Dialog.js

--- src/components/shared/Dialog.js
+++ src/components/shared/Dialog.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-    Modal
-} from 'react-bootstrap';
-import classNames from 'classnames';
+import { Modal } from 'react-bootstrap';
 
 import BaseComponent from './BaseComponent';
 
