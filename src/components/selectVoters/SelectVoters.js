5a7bf9bb2488deab17e3ac530a1c907342b57340
greatdolphinls <great.dolphin.ls@gmail.com>
2019-06-09 19:39:05 +0300

    B2-140: Add VotersProgressBar in Shared component.

--- src/components/selectVoters/SelectVoters.js
+++ src/components/selectVoters/SelectVoters.js
@@ -6,49 +6,151 @@ import { Row, Col } from 'react-bootstrap';
 import BaseComponent from '../shared/BaseComponent';
 import Checkbox from '../shared/Checkbox';
 import SearchInput from '../shared/SearchInput';
+import VotersProgressBar from '../shared/VotersProgressBar';
 import ContentLayout from '../layout/ContentLayout';
 
 class SelectVoters extends BaseComponent {
     constructor() {
         super();
         this.state = {
-            voterLists: null
+            votersList: [
+                { 
+                    id: 1,
+                    name: 'Denis Damin 1',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                },
+                { 
+                    id: 2,
+                    name: 'Denis Damin 2',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                },
+                { 
+                    id: 3,
+                    name: 'Denis Damin 3',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                },
+                { 
+                    id: 4,
+                    name: 'Denis Damin 4',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                },
+                { 
+                    id: 5,
+                    name: 'Denis Damin 5',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                },
+                { 
+                    id: 6,
+                    name: 'Denis Damin 6',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                },
+                { 
+                    id: 7,
+                    name: 'Denis Damin 7',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                },
+                { 
+                    id: 8,
+                    name: 'Denis Damin 8',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                },
+                { 
+                    id: 9,
+                    name: 'Denis Damin 9',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                },
+                { 
+                    id: 10,
+                    name: 'Denis Damin 10',
+                    street: 'New work Sr. 1289',
+                    sex: 'Male'
+                }
+            ],
+            selectVoters: []
         }
     }
 
     checkHandler = event => {
         const { checked } = event.target;
-        console.log(checked);
+        checked && this.setState({selectVoters: this.state.votersList});
     }
 
     searchInputHandler = value => {
         console.log(value);
     }
 
+    clearSelectedVotersHandler = (id) => {
+        let { selectVoters } = this.state;
+
+        if (id === 'all') {
+            selectVoters = [];
+        } else {
+            const targetIndex = selectVoters.findIndex(voter => (
+                voter.id === id
+            ));
+
+            selectVoters = [
+                ...selectVoters.slice(0, targetIndex), 
+                ...selectVoters.slice(targetIndex + 1)
+            ];
+        }
+
+        this.setState({selectVoters});
+    }
+
+    nextHandler = () => {
+        console.log('nextHandler');
+    }
+
     render() {
+        const {selectVoters} = this.state;
         return (
             <ContentLayout>
                 <div className='btw-select-voters container'>
                     <Row>
                         <Col xs={12}>
-                            <div className='title'>Add 10 voters to your list</div>
-                            <div className='description'>
-                                Select 10 people among your social media friends or search
-                                for other people you know among all the voters of your district.
-                                Try to choose a few among regular voters, a few among infrequent voters,
-                                and a few unregistered voters.
-					        </div>
-                            <Checkbox onChange={(event) => this.checkHandler(event)} label='test check' />
-                            <SearchInput
-                                placeholder='Search by name or address'
-                                onChange={(value) => this.searchInputHandler(value)} />
-                            <SearchInput
-                                placeholder='Search by name or address'
-                                onChange={(value) => this.searchInputHandler(value)} />
+                            <Row>
+                                <Col xs={12}>
+                                    <div className='title'>Add 10 voters to your list</div>
+                                    <div className='description'>
+                                        Select 10 people among your social media friends or search
+                                        for other people you know among all the voters of your district.
+                                        Try to choose a few among regular voters, a few among infrequent voters,
+                                        and a few unregistered voters.
+					                </div>
+                                    <Checkbox onChange={(event) => this.checkHandler(event)} label='test check' />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12} sm={12} md={12} lg={9}>
+                                     <SearchInput
+                                        placeholder='Search by name or address'
+                                        onChange={(value) => this.searchInputHandler(value)} />
+                                </Col>
+                                <Col xs={12} sm={12} md={12} lg={3}>
+                                    {/* <VotersProgressBar 
+                                        selectVoters={selectVoters}
+                                        onClear={this.clearSelectedVotersHandler}
+                                        onNext={this.nextHandler} /> */}
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                 </div >
-            </ContentLayout>
+                <VotersProgressBar 
+                    selectVoters={selectVoters}
+                    onClear={this.clearSelectedVotersHandler}
+                    onNext={this.nextHandler} />
+            </ContentLayout >
         );
     }
 }
---
+++ src/components/shared/VotersProgressBar.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Row, Col } from 'react-bootstrap';
+
+import BaseComponent from './BaseComponent';
+import Button from './Button';
+
+class VotersProgressBar extends BaseComponent {
+
+    constructor() {
+        super();
+        this.state = {
+            zoom: false
+        }
+    }
+
+    clickHandler = () => {
+        const { selectVoters, onNext } = this.props;
+        selectVoters.length === 10 && onNext();
+    }
+
+    clearItemHandler = (id) => {
+        this.props.onClear(id);
+    }
+
+    clearAllHandler = () => {
+        const { selectVoters, onClear } = this.props;
+        selectVoters.length !== 0 && onClear('all');
+    }
+
+    zoomHandler = () => {
+        const { zoom } = this.state;
+        const { selectVoters } = this.props;
+
+        selectVoters.length !== 0 && this.setState({ zoom: !zoom });
+    }
+
+    votersListRender = () => {
+        const { selectVoters } = this.props;
+        const { zoom } = this.state;
+
+        return (
+            <Row className={classNames('voters-list-container', zoom && 'open')}>
+                {
+                    selectVoters.map((voter, i) => {
+                        return (
+                            <Col key={i} xs={12} sm={4} md={4} lg={12}>
+                                <div className='item'>
+                                    <div className='name'>
+                                        {voter.name}
+                                    </div>
+                                    <span className='fa fa-close' onClick={() => this.clearItemHandler(voter.id)} />
+                                </div>
+                            </Col>
+                        );
+                    })
+                }
+            </Row >
+        );
+    }
+
+    progressBarRender = () => {
+        const { selectVoters } = this.props;
+
+        return (
+            <div className='progress-container'>
+                {[...Array(10)].map((e, i) => {
+                    return <div key={i} className={classNames('item', selectVoters.length > i && 'active')} />
+                })}
+            </div>
+        );
+    }
+
+    render() {
+        const { selectVoters } = this.props;
+        return (
+            <div className='btw-voters-progress-bar'>
+                <div className='zoom-container'>
+                    <div className='zoom' onClick={this.zoomHandler} />
+                </div>
+                <div className='status-container'>
+                    <div className='status'>Added {selectVoters.length}/10:</div>
+                    <a
+                        className={classNames('clear', selectVoters.length !== 0 && 'active')}
+                        onClick={this.clearAllHandler}>
+                        Clear All
+                    </a>
+                </div>
+                {this.progressBarRender()}
+                {this.votersListRender()}
+                <div className='button-container'>
+                    <Button
+                        color='blue4'
+                        style={{ fontSize: 13 }}
+                        onClick={this.clickHandler}
+                        disabled={selectVoters.length < 10} >
+                        Next Step
+                    </Button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default VotersProgressBar;
