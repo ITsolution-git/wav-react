031ecb5e026bbfcd4d59399bdf802c380565ab1a
Volodymyr Vinichuk <vvinichuk@truveris.com>
2019-06-10 21:15:45 +0300

    typography component

--- src/components/shared/Typography.js
+++ src/components/shared/Typography.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import BaseComponent from './BaseComponent';
+import colors from '../../constants/Colors';
 
 class Typography extends BaseComponent {
+
+    resolveStyleObj = (color, lightColor, fontWeight) => {
+        return {
+            color: lightColor ? colors.secondary : color,
+            fontWeight
+        }
+    };
+
     render() {
-        const { variant = 'heading', color = '', isBold = false, children } = this.props;
-        return (
-            <span className='btw-typography'>
-                { children }
-            </span>
-        )
+        const {
+            // native props
+            className,
+            style,
+            children,
+
+            // custom props
+            variant = 'heading',
+            fontWeight,
+            color = '',
+            lightColor = false,
+            displayInline = false,
+            ...restProps
+        } = this.props;
+
+        const styles = {...style, ...this.resolveStyleObj(color, lightColor, fontWeight)};
+
+        return React.createElement(displayInline ? 'span' : 'div', {
+            ... {
+                className: cn('btw-typography', variant, className ),
+                style: styles,
+                children
+            }, ...restProps
+        });
     }
 }
 
 Typography.propTypes = {
   variant: PropTypes.oneOf(['heading', 'body', 'functional']),
   color: PropTypes.string,
-  isBold: PropTypes.boolean
+  lightColor: PropTypes.bool,
+  displayInline: PropTypes.bool,  
+  fontWeight: PropTypes.string
 };
 
 export default Typography;
---
+++ src/constants/Colors.js
@@ -0,0 +1,12 @@
+export default {
+    primary: '#4B70F3',
+    primaryHover: '#3B5DD9',
+    secondary: '#515973',
+    main: '#0A1333',
+    divider: '#E9EBF3',
+    error: '#ED5052',
+    background: '#FAFBFF',
+    success: '#20DDAD',
+    alert: '#FEC754',
+    white: '#FFFFFF'
+}
