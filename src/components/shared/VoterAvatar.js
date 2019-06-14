97fba6be30f0003c64b884807230161588552f8a
zejdt <tutzejd29@gmail.com>
2019-06-14 19:33:52 +0200

    voteravatar: created

---
+++ src/components/shared/VoterAvatar.js
@@ -0,0 +1,46 @@
+/**
+ * usage: <VoterAvatar size={100} initials='SG' src='https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png' color='error' />
+ * created at: 2019/06/14
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
+import BaseComponent from './BaseComponent';
+import colors from '../../constants/Colors';
+
+class VoterAvatar extends BaseComponent {
+
+    render() {
+        const { src, size, color, initials } =  this.props
+
+        return(
+            <div className={cn('btw-voter-avatar')} style={{width: size + 'px', height: size + 'px', borderColor: colors[color] }}>
+                {
+                    src ? <img className={cn('btw-voter-img')} src={src}/>
+                    : <span className={cn('btw-voter-avatar-initials')}>{initials}</span>
+                }
+            </div>
+        )
+    }
+}
+
+VoterAvatar.defaultProps = {
+    src: '',
+    size: 40,
+    color: 'success',
+    initials: ''
+};
+
+VoterAvatar.propTypes = {
+    // avatar URL
+    src: PropTypes.string,
+    // avatar width and height pixel
+    size: PropTypes.number,
+    //avatar color : error, alert, success
+    color: PropTypes.string,
+    //avatar initials
+    initials: PropTypes.string.isRequired
+};
+
+export default VoterAvatar;
