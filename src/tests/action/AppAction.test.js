c990c869ca0a311c3adf8e92eb63404ecd2addbb
Volodymyr Vinichuk <vvinichuk@truveris.com>
2019-06-20 17:43:37 +0300

    fixed unit tests, removed obsolete ones

--- src/tests/action/AppAction.test.js
+++ src/tests/action/AppAction.test.js
@@ -32,7 +32,7 @@ describe('getBtwUserProfile tests', () => {
 
 	it('it should dispatch a success', () => {
 		let mockAdapter = new MockAdapter(axios);
-		mockAdapter.onGet(`${config.apiHost}/api/v1/getUser`).reply(200, response);
+		mockAdapter.onPost(`${config.apiHost}/auth/getUser`).reply(200, response);
 
 		const expectedActions = [
             {
@@ -52,95 +52,3 @@ describe('getBtwUserProfile tests', () => {
 	})
 
 });
-
-/* Test Register */
-describe('btwRegister', () => {
-
-	/* Success Test */
-	describe('Success', () => {
-
-		const request = {
-			"username" : "username",
-			"password" : "password",
-			"email"    : "email@test.com",
-			"firstname": "first",
-			"lastname" : "last"
-		}
-
-		const response = {
-			"status"         : 200,
-			"message"        : "User information successfully registered",
-			"userInformation": {
-				"username": "testing",
-        		"email": "testUser@test.com",
-				"firstname": "john",
-				"lastname": "test",
-				"role": "captain",
-				"dateofbirth": "2000-01-22T03:39:04.459Z",
-				"address": "1550 Acme street",
-				"phonenumber": "7765564732"
-			}
-		}
-
-		it('it should dispatch a success', () => {
-			let mockAdapter = new MockAdapter(axios);
-			mockAdapter.onPost(`${config.apiHost}/user/login`).reply(200, {});
-			mockAdapter.onPost(`${config.apiHost}/user/register`).reply(200, response);
-
-			const expectedActions = [
-				{
-                    dataType: AppDataTypes.register,
-                    type: AppConstants.INITIALIZE_REQUEST
-				},
-				{
-                    dataType: AppDataTypes.signOn,
-                    type: AppConstants.INITIALIZE_REQUEST
-				}
-			];
-
-			const store = mockStore({response : []});
-			return store.dispatch(signUpWitMail( request ) ).then( () => {
-				expect(store.getActions()).to.deep.equal(expectedActions)
-			} )
-		})
-	})
-
-	/* Failure Test */
-	describe('Failure', () => {
-
-		const request = {
-			"username" : "username",
-			"password" : "password",
-			"email"    : "email@test.com",
-			"firstname": "first",
-			"lastname" : "last"
-		}
-
-		const error = {
-			"status"         : 400,
-			"message"        : "An error occurred on server"
-		}
-
-		it('it should dispatch a failure', () => {
-			let mockAdapter = new MockAdapter(axios);
-			mockAdapter.onPost(`${config.apiHost}/user/register`).reply(400,error);
-
-			const expectedActions = [
-                {
-                    dataType: AppDataTypes.register,
-                    type: AppConstants.INITIALIZE_REQUEST
-                },
-                {
-                    dataType: AppDataTypes.register,
-                    type: AppConstants.LOAD_DATA_FAILURE,
-					data: undefined,
-                    error
-                }
-			]
-			const store = mockStore({response : []});
-			return store.dispatch(signUpWitMail( request ) ).then( () => {
-				expect(store.getActions()).to.deep.equal(expectedActions)
-			})
-		})
-	})
-})
