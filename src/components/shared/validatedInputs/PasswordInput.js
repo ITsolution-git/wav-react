1f8659b50232d579a3f6061c45c9dbd4312d641d
greatdolphinls <great.dolphin.ls@gmail.com>
2019-06-26 10:37:33 +0300

    B2-224: add placeholder in changePassword page and change validation text and rule.

--- src/components/shared/validatedInputs/PasswordInput.js
+++ src/components/shared/validatedInputs/PasswordInput.js
@@ -5,17 +5,21 @@ import validationTypes from '../../../constants/ValidationTypes';
 import { validate } from '../../../utility/InputValidator';
 
 export default class PasswordInput extends InputBase {
-    render () {
+    render() {
         return <TextInput label='Password'
-                          type='password'
-                          id='password'
-                          validator={value => validate(validationTypes.password, value)}
-                          validatorError={
-                              <div className="btw-password-error">
-                                  <div id="requirements">Your Password must be at least 7 characters with one special character and one upper case letter</div>
-                              </div>
-                          }
-                          name={fields.password}
-                          {...this.props } />
+            type='password'
+            id='password'
+            validator={value => validate(validationTypes.password, value)}
+            validatorError={
+                <div className='btw-password-error'>
+                    <div id='requirements'>
+                        Your Password must be at least 6 characters
+                        with one special character and one upper case letter,
+                        one number
+                    </div>
+                </div>
+            }
+            name={fields.password}
+            {...this.props} />
     }
 }
