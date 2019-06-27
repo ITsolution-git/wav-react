d515315a35c27724dc440f2d0546c774f15b02e9
greatdolphinls <great.dolphin.ls@gmail.com>
2019-06-27 18:42:32 +0300

    B2-226: remove dot in changePassword page

--- src/components/auth/changePassword/changePassword.js
+++ src/components/auth/changePassword/changePassword.js
@@ -120,7 +120,7 @@ class ChangePassword extends BaseComponent {
                         Save New Password
                     </Button>
                     <Typography variant='body' className='remember'>
-                        Remembered? <Link to={routes.loginBySocial}>Log in.</Link>
+                        Remembered? <Link to={routes.loginBySocial}>Log in</Link>
                     </Typography>
                 </div>
             </div >
