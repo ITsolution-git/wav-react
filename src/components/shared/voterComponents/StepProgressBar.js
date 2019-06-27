f2815a19b662e27619135d8fef4c079d233c5594
itsolution-git <itsolution0820@gmail.com>
2019-06-27 18:50:42 -0400

    update default number steps and routes

--- src/components/shared/voterComponents/StepProgressBar.js
+++ src/components/shared/voterComponents/StepProgressBar.js
@@ -10,12 +10,12 @@ import PropTypes from 'prop-types';
 import cn from 'classnames'
 
 const StepProgressBar = (props) => {
-	const { numberOfSteps } = props
-	const tenArray = [...Array(10).keys()]
+	const { step } = props
+	const tenArray = [...Array(props.numberOfSteps).keys()]
 
 	return (
 		<div className='d-flex my-4'>
-			{tenArray.map((_, index) => <span className={cn('btw-one-step', index < numberOfSteps && 'active')}></span>)}
+			{tenArray.map((_, index) => <span className={cn('btw-one-step', index < step && 'active')}></span>)}
 		</div>
 	)
 }
@@ -24,10 +24,12 @@ const StepProgressBar = (props) => {
 StepProgressBar.propTypes = {
 	/* showing step number */
     numberOfSteps: PropTypes.number,
+    step: PropTypes.number,
 };
 
 StepProgressBar.defaultProps = {
-	numberOfSteps: 0
+	numberOfSteps: 10,
+	step: 0
 }
 
 export default StepProgressBar
