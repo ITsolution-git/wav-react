fa69afb8f1e926f53df137dac3c2175ff7d0c891
greatdolphinls <great.dolphin.ls@gmail.com>
2019-07-02 00:28:31 +0300

    B2-262: add page header

--- src/components/dashboard/leaderboard/Leaderboard.js
+++ src/components/dashboard/leaderboard/Leaderboard.js
@@ -1,28 +1,59 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
-import { BaseComponent } from '../../shared';
+import { BaseComponent, Typography, SvgIcon } from '../../shared';
 
 class Leaderboard extends BaseComponent {
 
     constructor(props) {
         super(props);
         this.state = {
+            performers: [
+                {
+                    id: 1,
+                    firstName: 'Denis',
+                    lastName: 'Damin',
+                    level: 'Captain',
+                    points: 35,
+                    activeTasks: 6,
+                    src: 'https://images.unsplash.com/photo-1520484033379-7f74cc7d7340?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+                },
+                {
+                    id: 2,
+                    firstName: 'Denis',
+                    lastName: 'Damin',
+                    level: 'Captain',
+                    points: 65,
+                    activeTasks: 56,
+                    src: 'https://images.unsplash.com/photo-1520484033379-7f74cc7d7340?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+                },
+                {
+                    id: 3,
+                    firstName: 'Denis',
+                    lastName: 'Damin',
+                    level: 'Captain',
+                    points: 45,
+                    activeTasks: 60,
+                    src: 'https://images.unsplash.com/photo-1520484033379-7f74cc7d7340?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+                }
+            ],
         }
     }
 
     render() {
         return (
-            <Container className='btw-captains-dashboard'>
-                <Row className='user-info-content'>
-                    <Col md={5} lg={6} className='main-title'>
+            <Container className='btw-leaderboard'>
+                <div className='page-header'>
+                    <Typography variant='body' className='header-url'>
+                        Home  /  <span>Leaderboard</span>
+                    </Typography>
+                    <Typography className='title'>
                         Leaderboard
-                    </Col>
-                    <Col md={7} lg={6} className='p-0'>
-                    </Col>
-                </Row>
+                        <SvgIcon name='place-1' className='gold-medal' />
+                    </Typography>
+                </div>
             </Container>
         )
     }
