6452033284dba6ba54bb2a122e32029a5473a583
greatdolphinls <great.dolphin.ls@gmail.com>
2019-07-03 01:36:09 +0300

    B2-309: update SubTaskItem.

--- src/components/shared/taskComponents/ActionItem.js
+++ src/components/shared/taskComponents/ActionItem.js
@@ -14,30 +14,47 @@ class ActionItem extends BaseComponent {
         this.props.onSelectTask(this.props.task);
     }
 
+    renderHeader = () => {
+        const { task } = this.props
+
+        return (
+            <div className='action-header'>
+                <div className='action-status'>
+                    <SvgIcon name={task.status === 'completed' ? 'action-status-completed' : 'action-status-inprogress'} />
+                    <Typography className='status-text' variant='functional' lightColor>
+                        {task.status === 'completed' ? 'Completed' : 'In progress'}
+                    </Typography>
+                </div>
+
+                <div className='action-points'>
+                    <SvgIcon name='medal' />
+                    <Typography className='points-text' variant='functional' lightColor>
+                        {task.points.score} / {task.points.total}
+                    </Typography>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         const { task, className } = this.props
 
         return (
             <Paper className={cn(className, 'btw-action-item')}>
                 <div onClick={this.handleClick}>
-                    <div className='action-header'>
-                        <div className='action-status'>
-                            <SvgIcon name={task.status === 'completed' ? 'action-status-completed' : 'action-status-inprogress'} />
-                            <Typography className='status-text' variant='functional' lightColor>{task.status === 'completed' ? 'Completed' : 'In progress'}</Typography>
-                        </div>
-
-                        <div className='action-points'>
-                            <SvgIcon name='medal' />
-                            <Typography className='points-text' variant='functional' lightColor>{task.points.score} / {task.points.total}</Typography>
-                        </div>
-                    </div>
-
-                    <Typography className='action-title' variant='functional'>{task.title}</Typography>
-                    <Typography className='action-duration' lightColor variant='functional'>{task.start_date} – {task.end_date}</Typography>
+                    {this.renderHeader()}
+                    <Typography className='action-title' variant='functional'>
+                        {task.title}
+                    </Typography>
+                    <Typography className='action-duration' lightColor variant='functional'>
+                        {task.start_date} – {task.end_date}
+                    </Typography>
                     <TaskProgressBar total={task.subTasks.length}
                         completedNumber={this.getCompletedTasksCount(task)}
                     />
-                    <Typography className='task-done' lightColor variant='functional'>Tasks done: {this.getCompletedTasksCount(task)} / {task.subTasks.length}</Typography>
+                    <Typography className='task-done' lightColor variant='functional'>
+                        Tasks done: {this.getCompletedTasksCount(task)} / {task.subTasks.length}
+                    </Typography>
                 </div>
             </Paper>
         );
