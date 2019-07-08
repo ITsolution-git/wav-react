41d0e2b8d7b2bb96dda30f7e08917be43c1cd4c7
greatdolphinls <great.dolphin.ls@gmail.com>
2019-07-08 07:48:29 +0300

    B2-359: remove logout button on mobile setting page

--- src/components/shared/settings/LeftSideMenu.js
+++ src/components/shared/settings/LeftSideMenu.js
@@ -6,21 +6,21 @@ import { Typography, ButtonLink } from '../index'
 import { isDesktop } from '../../../helpers/DeviceHelper'
 
 const LeftSideMenu = (props) => {
-	const [ active , setActive ] = useState('Profile')
+	const [active, setActive] = useState('Profile')
 	const onMenuHanlder = menu => () => {
 		setActive(menu)
 		props.onSetActiveMenu(menu)
 	}
 
 	return (
-		<div className={cn('d-flex btw-setting-menu', {'flex-column': isDesktop})}>
-			<div className={cn('d-flex btw-setting-sub-menu', {'flex-column': isDesktop})}>
-				<Typography variant='body' fontWeight='700' className={cn('h6', {'active': active === 'Profile'})} onClick={onMenuHanlder('Profile')}> Profile </Typography>
-				<Typography variant='body' fontWeight='700' className={cn('h6', {'active': active === 'Password'})} onClick={onMenuHanlder('Password')}> Password </Typography>
-				<Typography variant='body' fontWeight='700' className={cn('h6', {'active': active === 'Notification'})} onClick={onMenuHanlder('Notification')}> Notification </Typography>
+		<div className={cn('d-flex btw-setting-menu', { 'flex-column': isDesktop })}>
+			<div className={cn('d-flex btw-setting-sub-menu', { 'flex-column': isDesktop })}>
+				<Typography variant='body' fontWeight='700' className={cn('h6', { 'active': active === 'Profile' })} onClick={onMenuHanlder('Profile')}> Profile </Typography>
+				<Typography variant='body' fontWeight='700' className={cn('h6', { 'active': active === 'Password' })} onClick={onMenuHanlder('Password')}> Password </Typography>
+				<Typography variant='body' fontWeight='700' className={cn('h6', { 'active': active === 'Notification' })} onClick={onMenuHanlder('Notification')}> Notification </Typography>
 			</div>
 			<br />
-			<ButtonLink onClick={props.onLogout} label='Log out'/>
+			<ButtonLink onClick={props.onLogout} label='Log out' className='logout-button' />
 		</div>
 	)
 }
